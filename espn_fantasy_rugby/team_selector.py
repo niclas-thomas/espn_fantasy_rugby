6ba5e2d7d63292ea5225cb35923ca73db3d36d23
Niclas Thomas <niclas.thomas@gmail.com>
2019-12-31 17:52:26 +0000

    Include round to round constraint

--- espn_fantasy_rugby/team_selector.py
+++ espn_fantasy_rugby/team_selector.py
@@ -1,6 +1,8 @@
 import pulp
 import pandas
 
+from .points_predictor import get_results
+
 TEAM_COLORS = {
     'WAL':'red',
     'ENG':'white',
@@ -18,6 +20,27 @@ TEAM_FONT_COLORS = {
     'SCO':'white',
     'ITA':'white'
 }
+
+POS_THRESHOLDS = {
+    'OB': 3,
+    'C': 2,
+    'FH': 1,
+    'SH': 1,
+    'BR': 3,
+    'SR': 2,
+    'FR': 3
+}
+
+
+def get_initial_team(first_round_players, espn_data):
+    team = espn_data.loc[
+        espn_data['NAME'].isin(first_round_players)
+    ][['NAME', 'TEAM', 'POS']].drop_duplicates().reset_index(drop=True)
+
+    if len(team) == 15:
+        return team
+    else:
+        raise Exception('The selected team does not contain fifteen valid players')
 
 
 def create_pulp_varnames(x):
@@ -51,7 +74,7 @@ def create_team_selection_problem(player_forecasts):
     return prob
 
 
-def add_nationality_constraint(prob, player_forecasts, threshold):
+def add_nationality_constraint(prob, player_forecasts, max_num_from_country=3):
 
     wal_nation = {
         name: 1 if team == 'WAL' else 0 for name, team in zip(player_forecasts['VARNAME'], player_forecasts['TEAM'])
@@ -73,22 +96,22 @@ def add_nationality_constraint(prob, player_forecasts, threshold):
     }
 
     prob += pulp.lpSum(
-        [wal_nation[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]) <= threshold, "WAL_Requirement"
+        [wal_nation[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]) <= max_num_from_country, "WAL_Requirement"
     prob += pulp.lpSum(
-        [eng_nation[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]) <= threshold, "ENG_Requirement"
+        [eng_nation[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]) <= max_num_from_country, "ENG_Requirement"
     prob += pulp.lpSum(
-        [ire_nation[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]) <= threshold, "IRE_Requirement"
+        [ire_nation[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]) <= max_num_from_country, "IRE_Requirement"
     prob += pulp.lpSum(
-        [fra_nation[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]) <= threshold, "FRA_Requirement"
+        [fra_nation[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]) <= max_num_from_country, "FRA_Requirement"
     prob += pulp.lpSum(
-        [sco_nation[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]) <= threshold, "SCO_Requirement"
+        [sco_nation[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]) <= max_num_from_country, "SCO_Requirement"
     prob += pulp.lpSum(
-        [ita_nation[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]) <= threshold, "ITA_Requirement"
+        [ita_nation[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]) <= max_num_from_country, "ITA_Requirement"
 
     return prob
 
 
-def add_position_constraint(prob, player_forecasts, pos_caps):
+def add_position_constraint(prob, player_forecasts):
 
     ob_position = {
         name: 1 if pos == 'OB' else 0 for name, pos in zip(player_forecasts['VARNAME'], player_forecasts['POS'])
@@ -113,19 +136,34 @@ def add_position_constraint(prob, player_forecasts, pos_caps):
     }
 
     prob += pulp.lpSum(
-        [ob_position[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]) <= pos_caps['OB'], "OB"
+        [ob_position[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]) == POS_THRESHOLDS['OB'], "OB"
     prob += pulp.lpSum(
-        [c_position[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]) <= pos_caps['C'], "C"
+        [c_position[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]) == POS_THRESHOLDS['C'], "C"
     prob += pulp.lpSum(
-        [fh_position[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]) <= pos_caps['FH'], "FH"
+        [fh_position[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]) == POS_THRESHOLDS['FH'], "FH"
     prob += pulp.lpSum(
-        [sh_position[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]) <= pos_caps['SH'], "SH"
+        [sh_position[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]) == POS_THRESHOLDS['SH'], "SH"
     prob += pulp.lpSum(
-        [br_position[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]) <= pos_caps['BR'], "BR"
+        [br_position[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]) == POS_THRESHOLDS['BR'], "BR"
     prob += pulp.lpSum(
-        [sr_position[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]) <= pos_caps['SR'], "SR"
+        [sr_position[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]) == POS_THRESHOLDS['SR'], "SR"
     prob += pulp.lpSum(
-        [fr_position[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]) <= pos_caps['FR'], "FR"
+        [fr_position[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]) == POS_THRESHOLDS['FR'], "FR"
+
+    return prob
+
+
+def add_round_changes_constraint(prob, player_forecasts, previous_team, max_num_changes=3):
+
+    number_of_players_to_keep = 15 - max_num_changes
+
+    number_changes = {
+        name: 1 if name in previous_team else 0 for name in player_forecasts['VARNAME']
+    }
+
+    prob += pulp.lpSum(
+        [number_changes[i] * prob.variablesDict()[i] for i in prob.variablesDict().keys()]
+    ) >= number_of_players_to_keep, "Max_number_of_changes"
 
     return prob
 
@@ -139,6 +177,9 @@ def get_team(prob, player_forecasts):
     player_variables = {str(value): key for key, value in zip(players, prob.variables())}
     prob.solve()
 
+    if pulp.LpStatus[prob.status] != 'Optimal':
+        print("No optimal solution found: check your initial team")
+
     team = pandas.DataFrame()
 
     for v in prob.variables():
@@ -149,7 +190,6 @@ def get_team(prob, player_forecasts):
                 'TEAM': [player_nationality[name]],
                 'POS': [player_positions[name]]
             }))
-    team = team.merge(player_forecasts[['NAME', 'MDP']], on='NAME')
 
     return team
 
@@ -159,12 +199,57 @@ def get_predicted_team_points(prob):
     return pulp.value(prob.objective)
 
 
-def get_actual_team_points(team):
+def get_predicted_player_points(team, players_forecast):
 
-    return team['MDP'].sum()
+    team = team.merge(players_forecast[['NAME', 'PREDICTION']], on='NAME')
+
+    return team
+
+
+def get_actual_player_points(team, espn_data, tournament_round):
+
+    mdp_round_data = espn_data.loc[espn_data['ROUND'] == tournament_round]
+    team = team.merge(mdp_round_data[['NAME', 'MDP']], on='NAME')
+
+    return team
+
+
+def simulate_tournament(espn_data, team, features, target):
+    team_each_round = pandas.DataFrame()
+
+    for tournament_round in [1, 2, 3, 4, 5]:
+
+        previous_players = [create_pulp_varnames(name) for name in team['NAME']]
+
+        if tournament_round == 1:
+            team_predictions = team.copy()
+            team_predictions['PREDICTION'] = 3
+
+        else:
+            player_forecasts = get_results(
+                espn_data, features, target, tournament_round
+            )
+            problem = create_team_selection_problem(player_forecasts)
+            problem = add_nationality_constraint(problem, player_forecasts)
+            problem = add_position_constraint(problem, player_forecasts)
+            problem = add_round_changes_constraint(problem, player_forecasts, previous_players)
+
+            team = get_team(problem, player_forecasts)
+            team_predictions = get_predicted_player_points(team, player_forecasts)
+
+        team_actuals = get_actual_player_points(team, espn_data, tournament_round)
+        team_predictions_and_actuals = team_actuals.merge(
+            team_predictions[['NAME', 'PREDICTION']],
+            on='NAME'
+        )
+        team_predictions_and_actuals['ROUND'] = tournament_round
+        team_each_round = team_each_round.append(team_predictions_and_actuals)
+
+    return team_each_round
 
 
 def show_pitch(ax):
+
     # Pitch Markings
     ax.axhline(4, linestyle='-', color='white')
     ax.axhline(1.5, linestyle='-', color='white')
@@ -189,10 +274,11 @@ def show_pitch(ax):
     return ax
 
 
-def show_team(team, ax):
+def show_team(team_actuals, ax):
+
     show_pitch(ax)
 
-    team = team.sort_values('POS').reset_index(drop=True)
+    team_actuals = team_actuals.sort_values(['POS', 'NAME']).reset_index(drop=True)
 
     coords = [
         (1, 5.5),
@@ -212,7 +298,7 @@ def show_team(team, ax):
         (2.5, 6.25)
     ]
 
-    for ix, row in team.iterrows():
+    for ix, row in team_actuals.iterrows():
         ax.text(
             coords[ix][0],
             coords[ix][1],
